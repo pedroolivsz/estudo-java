03337482363f665498109587bb039fccaf8aca16
João Pedro <joaopedropereira2209@gmail.com>
2026-02-18 18:03:03 -0300

    exception handling exercises

---
+++ src/main/java/com/io/github/pedroolivsz/exceções/application/Program.java
@@ -0,0 +1,42 @@
+package com.io.github.pedroolivsz.exceções.application;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        method1();
+
+        System.out.println("End of program");
+    }
+
+    public static void method1() {
+        System.out.println("***METHOD1 START***");
+
+        method2();
+
+        System.out.println("***METHOD1 END***");
+    }
+
+    public static void method2() {
+        System.out.println("***METHOD2 START***");
+
+        Scanner scanner = new Scanner(System.in);
+
+        try {
+            String[] vect = scanner.nextLine().split(" ");
+            int position = scanner.nextInt();
+            System.out.println(vect[position]);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Invalid position");
+            e.printStackTrace();
+            scanner.next();
+        } catch (InputMismatchException e) {
+            System.out.println("Input error");
+        }
+
+        scanner.close();
+
+        System.out.println("***METHOD2 END***");
+    }
+}
